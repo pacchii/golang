61717da8b89c2cfdfda9e756a1517d54d36565b3
pacchii <prashantghiwari@gmail.com>
2026-02-20 10:23:11 +0530

    Adding author 2

--- ToddMcLeod/author.go
+++ ToddMcLeod/author.go
@@ -1,1 +1,9 @@
 package main
+
+func Author() string {
+	return "Prashant G"
+}
+
+func place() string {
+	return Author() + "Bangalore"
+}
---
+++ ToddMcLeod/go.mod
@@ -0,0 +1,3 @@
+module github.com/pacchii/golang
+
+go 1.25.0
